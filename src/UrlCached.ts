a78f6702504e40f6fb1f7bd18b7a16854d8107c1
ghislaingenay <ghislain.gny@gmail.com>
2024-03-18 14:58:44 +0700

    feat: prepare cache using json

---
+++ src/UrlCached.ts
@@ -0,0 +1,65 @@
+import fs from "fs";
+import { FileExplorer } from "./FileExplorer";
+
+type UrlCountRange = {
+  max: number;
+  min: number;
+  count: number;
+};
+
+// DONE USING REDIS
+export class UrlCached {
+  LOT = 1000;
+  FILE_NAME = "cache.json";
+
+  // Related to count
+  getUrlCount(): UrlCountRange {
+    if (FileExplorer.isPathValid(this.FILE_NAME) === false)
+      throw new Error("File not found");
+
+    const value = fs.readFileSync(this.FILE_NAME, "utf8");
+    //   .split("\n")
+    //   .map((value: string) => value.trim())
+    //   .reduce((acc: UrlCountRange, value: string) => {
+    //     const [key, val] = value.split("=");
+    //     acc[key as keyof UrlCountRange] = Number(val);
+    //     return acc;
+    //   }, {} as UrlCountRange);
+    return JSON.parse(value);
+  }
+
+  get currentCount() {
+    return this.getUrlCount().count;
+  }
+
+  private updateMaxMinCount(countData?: UrlCountRange) {
+    const data = countData || this.getUrlCount();
+    const [newMin, newMax] = [data.min + this.LOT, data.min + this.LOT * 2];
+    fs.writeFileSync(
+      this.FILE_NAME,
+      JSON.stringify({
+        count: newMin,
+        max: newMax,
+        min: newMin,
+      }),
+      "utf8"
+    );
+  }
+
+  updateCount() {
+    const countData = this.getUrlCount();
+    const newCount = this.currentCount + 1;
+    if (newCount > countData.max) this.updateMaxMinCount(countData);
+    fs.writeFileSync(
+      this.FILE_NAME,
+      JSON.stringify({ ...this.getUrlCount(), count: newCount }),
+      "utf8"
+    );
+  }
+
+  set cache(data: UrlCountRange) {
+    fs.writeFileSync(this.FILE_NAME, JSON.stringify(data), "utf8");
+  }
+}
+
+export const countCache = new UrlCached();
