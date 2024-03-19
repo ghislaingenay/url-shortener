97c0ab21f0a0da384a9e9b9ddeabd06a290156e6
ghislaingenay <ghislain.gny@gmail.com>
2024-03-19 11:12:38 +0700

    feat: add basic error handling

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,47 @@ app.use(bodyParser.json());
 
 // Get all urls
 app.get("/", (req, res) => {
-  const urlService = new URLShortener();
-  res.json({ result: true, data: urlService.getAllUrls() });
+  try {
+    const urlService = new URLShortener();
+    res.status(200).json({ result: true, data: urlService.getAllUrls() });
+  } catch (err: unknown) {
+    res.status(400).json({ result: false, message: (err as Error).message });
+  }
 });
 
 app.get("/:mode/:id", (req, res) => {
-  const { mode, id } = req.params;
-  const urlService = new URLShortener();
-  if (mode !== "id" && mode !== "url") throw new Error("Invalid mode");
+  try {
+    const { mode, id } = req.params;
+    const urlService = new URLShortener();
+    if (mode !== "id" && mode !== "url") throw new Error("Invalid mode");
 
-  const setLongUrlParams = ({
-    mode,
-    id,
-  }: {
-    mode: "url" | "id";
-    id: string;
-  }) => {
-    if (mode === "id") return { id: Number(id), mode, shortUrl: undefined };
-    return { shortUrl: id, mode, id: undefined };
-  };
-  const params = setLongUrlParams({ mode, id });
-  const longUrl = urlService.getLongUrl(params);
-  res.json({ result: true, data: longUrl });
+    const setLongUrlParams = ({
+      mode,
+      id,
+    }: {
+      mode: "url" | "id";
+      id: string;
+    }) => {
+      if (mode === "id") return { id: Number(id), mode, shortUrl: undefined };
+      return { shortUrl: id, mode, id: undefined };
+    };
+    const params = setLongUrlParams({ mode, id });
+    const longUrl = urlService.getLongUrl(params);
+    res.status(200).json({ result: true, data: longUrl });
+  } catch (err: unknown) {
+    res.status(400).json({ result: false, message: (err as Error).message });
+  }
 });
 
 app.post("/", (req, res) => {
-  const longUrl = req.body.longUrl;
-  const urlService = new URLShortener();
-  const createdUrl = urlService.createURL(longUrl);
-  res.json({ result: createdUrl });
+  try {
+    const longUrl = req.body.longUrl;
+    const urlService = new URLShortener();
+    const createdUrl = urlService.createURL(longUrl);
+    res.status(201).json({ data: createdUrl, result: true });
+  } catch (err: unknown) {
+    res.status(400).json({ result: false, message: (err as Error).message });
+  }
 });
 
 app.listen(port, () => {
